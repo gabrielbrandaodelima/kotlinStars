9fba8660db4ff310da18d0ccb1a61d649f786528
gabriel <gabriel.brandao@aluno.ufop.edu.br>
2021-01-20 10:43:56 -0300

    #First Commit: Init Git

---
+++ app/src/main/java/com/gabriel/kotlinstars/application/Application.kt
@@ -0,0 +1,10 @@
+package com.gabriel.kotlinstars.application
+
+import android.app.Application
+
+class Application : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
