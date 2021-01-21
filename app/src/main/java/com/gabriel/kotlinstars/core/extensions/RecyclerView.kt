05df9502de2dcb0f5e914164bf020d1b5b6bc98c
gabriel <gabriel.brandao@aluno.ufop.edu.br>
2021-01-21 00:52:20 -0300

    #Feat: Created set up paging on RecyclerView.kt

--- app/src/main/java/com/gabriel/kotlinstars/core/extensions/RecyclerView.kt
+++ app/src/main/java/com/gabriel/kotlinstars/core/extensions/RecyclerView.kt
@@ -1,6 +1,7 @@
 package com.gabriel.kotlinstars.core.extensions
 
 import android.content.Context
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -17,5 +18,36 @@ fun RecyclerView?.setUpRecyclerView(
         }
 
         adapterSetCallback(this)
+    }
+}
+
+fun RecyclerView?.setUpPaging(
+    needsLoading: MutableLiveData<Boolean>,
+    refreshList: () -> Unit) {
+    this?.apply {
+        addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (dy > 0)
+                //check for scroll down
+                {
+                    (recyclerView.layoutManager?.let { it as LinearLayoutManager })?.let {
+                        val visItemCount = it.childCount
+                        val totItemCount = it.itemCount
+                        val pastItemCount = it.findFirstVisibleItemPosition()
+
+                        needsLoading.value?.let { reload ->
+                            if (reload) {
+                                if (visItemCount + pastItemCount >= totItemCount) {
+                                    needsLoading.value = false
+                                    refreshList()
+                                }
+                            }
+
+                        }
+                    }
+
+                }
+            }
+        })
     }
 }
