d32bd7b22bdb5ca05c44792d04b632272f49c75c
gabriel <gabriel.brandao@aluno.ufop.edu.br>
2021-01-20 22:42:56 -0300

    #Feat: Created item_git_repos.xml and set up navigation structure

--- app/src/main/java/com/gabriel/kotlinstars/features/ui/view/GitReposActivity.kt
+++ app/src/main/java/com/gabriel/kotlinstars/features/ui/view/GitReposActivity.kt
@@ -1,6 +1,9 @@
 package com.gabriel.kotlinstars.features.ui.view
 
 import android.os.Bundle
+import androidx.navigation.NavDestination
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import com.gabriel.kotlinstars.R
 import com.gabriel.kotlinstars.core.platform.BaseActivity
 
@@ -9,4 +12,5 @@ class GitReposActivity : BaseActivity(R.layout.activity_git_repos) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
 }
