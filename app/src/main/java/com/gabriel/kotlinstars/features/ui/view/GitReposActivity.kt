a6cb7665af00fbe394a3ae1cd06a7bd4210142f7
gabriel <gabriel.brandao@aluno.ufop.edu.br>
2021-01-20 22:31:32 -0300

    #Feat: Created fragment_git_repo_detail.xml

--- app/src/main/java/com/gabriel/kotlinstars/features/ui/view/GitReposActivity.kt
+++ app/src/main/java/com/gabriel/kotlinstars/features/ui/view/GitReposActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import com.gabriel.kotlinstars.R
 import com.gabriel.kotlinstars.core.platform.BaseActivity
 
-class GitReposActivity : BaseActivity(R.layout.git_repos_activity) {
+class GitReposActivity : BaseActivity(R.layout.activity_git_repos) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/gabriel/kotlinstars/features/ui/view/GitReposFragment.kt
+++ app/src/main/java/com/gabriel/kotlinstars/features/ui/view/GitReposFragment.kt
@@ -10,7 +10,7 @@ import com.gabriel.kotlinstars.R
 import com.gabriel.kotlinstars.core.platform.BaseFragment
 import com.gabriel.kotlinstars.features.ui.viewmodel.GitReposViewModel
 
-class GitReposFragment : BaseFragment(R.layout.git_repos_fragment) {
+class GitReposFragment : BaseFragment(R.layout.fragment_git_repos) {
 
     private lateinit var viewModel: GitReposViewModel
 
