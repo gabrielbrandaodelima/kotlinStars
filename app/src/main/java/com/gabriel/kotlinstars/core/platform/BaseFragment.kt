6a5a6a79c00d359ecf5d86a91bf462cad10cf286
gabriel <gabriel.brandao@aluno.ufop.edu.br>
2021-01-20 23:32:45 -0300

    #Feat: handling failure on BaseFragment.kt

--- app/src/main/java/com/gabriel/kotlinstars/core/platform/BaseFragment.kt
+++ app/src/main/java/com/gabriel/kotlinstars/core/platform/BaseFragment.kt
@@ -2,13 +2,14 @@ package com.gabriel.kotlinstars.core.platform
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import javax.inject.Inject
 
 
-abstract class BaseFragment(@LayoutRes layoutIdRes : Int = 0) : Fragment(layoutIdRes) {
+abstract class BaseFragment(@LayoutRes layoutIdRes: Int = 0) : Fragment(layoutIdRes) {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -17,5 +18,10 @@ abstract class BaseFragment(@LayoutRes layoutIdRes : Int = 0) : Fragment(layoutI
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+    }
+
+    protected fun handleFailure(failure: String?) {
+
+        Toast.makeText(requireContext(), failure, Toast.LENGTH_SHORT).show()
     }
 }
