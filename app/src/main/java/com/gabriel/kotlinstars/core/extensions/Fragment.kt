cd6e81eb7aaf01f07522867f897114190105ca36
gabriel <gabriel.brandao@aluno.ufop.edu.br>
2021-01-20 23:32:20 -0300

    #Feat: Created Fragment.kt and Lifecycle.kt

---
+++ app/src/main/java/com/gabriel/kotlinstars/core/extensions/Fragment.kt
@@ -0,0 +1,12 @@
+package com.gabriel.kotlinstars.core.extensions
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+
+inline fun <reified T : ViewModel> Fragment.viewModel(factory: ViewModelProvider.Factory, body: T.() -> Unit): T {
+    val vm = ViewModelProvider(this, factory)[T::class.java]
+    vm.body()
+    return vm
+}
